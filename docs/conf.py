b3e081ddc52ab8159bf22fda1ecf4aa1d5d68c38
michaelhsu <michaelhsu170@gmail.com>
2021-09-20 11:11:52 +0900

    update

--- docs/conf.py
+++ docs/conf.py
@@ -64,6 +64,9 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named 'default.css' will overwrite the builtin 'default.css'.
 html_static_path = ['_static']
 
+def setup(app):
+   app.add_css_file("custom.css")
+
 from os import getenv
 
 sphinx_md_useGitHubURL = True
